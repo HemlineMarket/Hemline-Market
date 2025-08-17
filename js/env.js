45c71bb99b517f86aa163fbbfd1569d36cf9afa9
HemlineMarket <hello@hemlinemarket.com>
2025-08-17 11:15:44 -0400

    Create env.js
    env: add Supabase URL and anon key for client

---
+++ js/env.js
@@ -0,0 +1,5 @@
+/* js/env.js */
+window.HEMLINE_ENV = {
+  SUPABASE_URL: "https://clkizksbvxjkoatdajgd.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsa2l6a3Nidnhqa29hdGRhamdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2ODAyMDUsImV4cCI6MjA3MDI1NjIwNX0.m3wd6UAuqxa7BpcQof9mmzd8zdsmadwGDO0x7-nyBjI"
+};
