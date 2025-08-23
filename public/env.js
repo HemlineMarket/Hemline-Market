85b52ce2193e784c61e64125aa53d8e6aafd6458
HemlineMarket <hello@hemlinemarket.com>
2025-08-22 20:18:04 -0400

    Create env.js

---
+++ public/env.js
@@ -0,0 +1,3 @@
+// Hemline Market – browser env (public)
+window.SUPABASE_URL = "https://clkizksbvxjkoatdajgd.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsa2l6a3Nidnhqa29hdGRhamdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2ODAyMDUsImV4cCI6MjA3MDI1NjIwNX0.m3wd6UAuqxa7BpcQof9mmzd8zdsmadwGDO0x7-nyBjI";
