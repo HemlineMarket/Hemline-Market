fead50853f6be4ef90d4cfbb6108b31764980b6c
HemlineMarket <hello@hemlinemarket.com>
2025-08-31 00:15:36 -0400

    Update env.js

--- public/env.js
+++ public/env.js
@@ -1,3 +1,5 @@
-// Hemline Market – browser env (public)
-window.SUPABASE_URL = "https://clkizksbvxjkoatdajgd.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsa2l6a3Nidnhqa29hdGRhamdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2ODAyMDUsImV4cCI6MjA3MDI1NjIwNX0.m3wd6UAuqxa7BpcQof9mmzd8zdsmadwGDO0x7-nyBjI";
+/* public/env.js */
+window.__env = Object.assign({}, window.__env, {
+  SUPABASE_URL: 'https://clkizksbvxjkoatdajgd.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsa2l6a3Nidnhqa29hdGRhamdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2ODAyMDUsImV4cCI6MjA3MDI1NjIwNX0.m3wd6UAuqxa7BpcQof9mmzd8zdsmadwGDO0x7-nyBjI'
+});
