cfce558fa712cbbda7295ebf3e5d7734d79ebe0d
HemlineMarket <hello@hemlinemarket.com>
2025-12-02 20:15:43 -0500

    Update threadtalk.js

--- public/scripts/threadtalk.js
+++ public/scripts/threadtalk.js
@@ -1463,7 +1463,7 @@
   function escapeHtml(str) {
     return String(str || "")
       .replace(/&/g, "&amp;")
-      .replace(/<//g, "&lt;")
+      .replace(/</g, "&lt;");
       .replace(/>/g, "&gt;")
       .replace(/"/g, "&quot;");
   }
