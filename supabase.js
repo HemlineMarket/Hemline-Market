8d16258f7ca1f0ecc81111c2d459cf6f57a56a1a
HemlineMarket <hello@hemlinemarket.com>
2025-12-27 12:47:19 -0500

    Delete supabase.js

--- supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-// Project URL from Supabase
-const SUPABASE_URL = 'https://clkizksbvxjkoatdajgd.supabase.co'
-
-// Anon public key from Supabase
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsa2l6a3Nidnhqa29hdGRhamdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2ODAyMDUsImV4cCI6MjA3MDI1NjIwNX0.m3wd6UAuqxa7BpcQof9mmzd8zdsmadwGDO0x7-nyBjI'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
