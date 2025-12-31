035f3d5e411ff25bd77664a8e9cd794acef4a52c
HemlineMarket <hello@hemlinemarket.com>
2025-12-31 18:06:37 +0000

    Update refund.js

--- api/admin/refund.js
+++ api/admin/refund.js
@@ -16,7 +16,7 @@ function auth(req, res) {
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-07-30.basil',
+  apiVersion: '2024-06-20',
 });
 
 async function findSession(order_id) {
